8b3a4bf1bc6cda29dcacc52934abdb279a0890af
Vinícius Sousa <vinicius.antunes@proton.me>
2023-10-17 11:31:56 -0300

    update smmcts.py

--- models/smmcts.py
+++ models/smmcts.py
@@ -71,6 +71,18 @@ class Node:
 
         return (self.ucb1(0),self.ucb1(1))
     
+    def get_win_rate(self, player:bool) -> float:
+        """returns the win rate for the given player
+
+        :param player: 0 for player 1, 1 for player 2
+        :type player: bool
+        :return: win rate
+        :rtype: float
+        """        
+        if self.N == 0:
+            return 0
+        return self.Q[player]/self.N
+    
     def get_root(self):
         """returns the root node
 
@@ -97,6 +109,8 @@ class MCTS:
         self.root = Node((0,0),None)
         self.node_count = 0
         self.num_rollout = 0
+        self.curr_node = self.root
+        self.curr_game = self.root_game
 
     def legal_moves(self,game: s.Surround) -> list[tuple[int]]:
         """returns a list of legal moves for both players
@@ -158,7 +172,7 @@ class MCTS:
             return False
         children = [Node(move,parent) for move in self.legal_moves(game)]
         parent.add_children(children)
-
+        self.node_count += len(children)
         return True
     
     def select_node(self) -> tuple[Node,s.Surround]:
@@ -189,7 +203,34 @@ class MCTS:
             game.step(node.move)
         
         return node, game
+    def select_node_curr(self) -> tuple[Node,s.Surround]:
+        """select a node to expand from curr_game
 
+        :return: node to expand, game state
+        :rtype: tuple[Node,s.Surround]
+        """        
+        node = self.curr_node
+        game = deepcopy(self.curr_game)
+
+        while len(node.children):
+            children = node.children.values()
+            max_value1 = max(children, key=lambda child: child.value()[0]).value()[0]
+            max_value2 = max(children, key=lambda child: child.value()[1]).value()[1]
+            max_nodes1 = [child for child in children if child.value()[0] == max_value1]
+            max_nodes2 = [child for child in children if child.value()[1] == max_value2]
+            node1 = random.choice(max_nodes1).move[0]
+            node2 = random.choice(max_nodes2).move[1]
+            node = node.children[(node1,node2)]
+            game.step(node.move)
+
+            if node.N == 0:
+                return node, game
+        
+        if self.expand(node, game):            
+            node = random.choice(list(node.children.values()))
+            game.step(node.move)
+        
+        return node, game
     def roll_out(self, game: s.Surround) -> int:
         """roll out the game until the end
 
@@ -205,9 +246,47 @@ class MCTS:
                 _,_,_,lose1,lose2 = game.step(random.choice(moves))
             else:
                 lose1, lose2 = True, True
-        
-        return [int(not lose1), int(not lose2)]
+        if lose1 and lose2:
+            return [0,0]
+        if lose1:
+            return [-1,1]
+        if lose2:
+            return [1,-1]
     
+    def roll_out2(self, game: s.Surround) -> int:
+        lose1, lose2, = False, False
+        old_moves = []
+        while not lose1 and not lose2:
+            moves = self.legal_moves(game)
+            moves1 = [move[0] for move in moves]
+            moves2 = [move[1] for move in moves]
+            if len(moves):
+                if len(old_moves):
+                    if old_moves[0] in moves1:
+                        p = random.random()
+                        if p < 0.68:
+                            move1 = old_moves[0]
+                        else:
+                            move1 = random.choice(moves1)
+                    if old_moves[1] in moves2:
+                        p = random.random()
+                        if p < 0.68:
+                            move2 = old_moves[1]
+                        else:
+                            move2 = random.choice(moves2)
+                else:
+                    move1 = random.choice(moves1)
+                    move2 = random.choice(moves2)
+                _,_,_,lose1,lose2 = game.step((move1,move2))
+                old_moves = [move1,move2]
+            else:
+                lose1, lose2 = True, True
+        if lose1 and lose2:
+            return [0,0]
+        if lose1:
+            return [-1,1]
+        if lose2:
+            return [1,-1]
     def back_propagate(self, node: Node, outcome: tuple[int]) -> None:
         """back propagate the outcome of the game
 
@@ -233,86 +312,99 @@ class MCTS:
         
         for _ in range(count):
             node, game = self.select_node()
-            outcome = self.roll_out(game)
+            outcome = self.roll_out2(game)
             self.back_propagate(node, outcome)
             self.num_rollout += 1
         
+    def search_curr(self, count: int = 50) -> None:
+        """Grow tree
+
+        :param count: number of times to perform tree growth, defaults to 50
+        :type count: int, optional
+        :return: best move, time used
+        :rtype: tuple[tuple[int],float]
+        """        
+        
+        for _ in range(count):
+            node, game = self.select_node_curr()
+            outcome = self.roll_out2(game)
+            self.back_propagate(node, outcome)
+            self.num_rollout += 1
     
     
     def move(self, move: tuple[int]) -> None:
-        """move the root to the child correspon10000ding to the move
+        """move the curr to the child corresponding to the move
 
         :param move: move to be performed
         :type move: tuple[int]
         """       
-        if self.game_over(self.root_game):
-            self.root_game.reset()
-            self.root = self.root.get_root()
-        if move in self.root.children:
-            self.root_game.step(move)
-            self.root = self.root.children[move]
+        if self.game_over(self.curr_game):
+            self.curr_game.reset()
+            self.curr_node = self.root
+        if move in self.curr_node.children:
+            self.curr_game.step(move)
+            self.curr_node = self.curr_node.children[move]
         else:
-            self.root_game.step(move)
-            if not self.game_over(self.root_game):
-                self.expand(self.root, self.root_game)
-                self.search()
-        if self.game_over(self.root_game):
-            self.root_game.reset()
-            self.root = self.root.get_root()
+            self.curr_game.step(move)
+            if not self.game_over(self.curr_game):
+                self.search_curr()
+        if self.game_over(self.curr_game):
+            self.curr_game.reset()
+            self.curr_node = self.root
         # raise Exception("Move not in children")
 
     def best_move(self) -> tuple[int,int]:
-        """returns the best move for each player
+        """returns the best move for each player for curr_game
 
         :return: best move for each player
         :rtype: tuple[int,int]
         """        
-        if self.game_over(self.root_game):
+        if self.game_over(self.curr_game):
             return None
-        if self.root.children == {}:
-            if self.legal_moves(self.root_game) == []:
+        if self.curr_node.children == {}:
+            if self.legal_moves(self.curr_game) == []:
                 return None
-            self.search()
-        max_value1 = max(self.root.children.values(), key=lambda child: child.Q[0]/child.N if child.N else 0)
-        max_value2 = max(self.root.children.values(), key=lambda child: child.Q[1]/child.N if child.N else 0)
-        max_value1 = max_value1.Q[0]/max_value1.N if max_value1.N else 0
-        max_value2 = max_value2.Q[1]/max_value2.N if max_value2.N else 0
+            self.search_curr(50)
+        max_value1 = max(self.curr_node.children.values(), key=lambda child: child.get_win_rate(0)).get_win_rate(0)
+        max_value2 = max(self.curr_node.children.values(), key=lambda child: child.get_win_rate(1)).get_win_rate(1)
         if max_value1 == 0:
-            max_nodes1 = [child for child in self.root.children.values()]
+            max_nodes1 = [child for child in self.curr_node.children.values()]
         else:
-            max_nodes1 = [child for child in self.root.children.values() if child.Q[0]/child.N == max_value1]
+            max_nodes1 = [child for child in self.curr_node.children.values() if child.Q[0]/child.N == max_value1]
         if max_value2 == 0:
-            max_nodes2 = [child for child in self.root.children.values()]
+            max_nodes2 = [child for child in self.curr_node.children.values()]
         else:
-            max_nodes2 = [child for child in self.root.children.values() if child.Q[1]/child.N == max_value2]
+            max_nodes2 = [child for child in self.curr_node.children.values() if child.Q[1]/child.N == max_value2]
         best_child1 = random.choice(max_nodes1).move[0]
         best_child2 = random.choice(max_nodes2).move[1]
 
         return (best_child1, best_child2)
     
-    def train(self, num_games: int = 1) -> None:
-        """train the model
+    def play(self, num_games: int = 1) -> None:
+        """play num_games games
 
         :param num_games: number of games played, defaults to 1
         :type num_games: int, optional
         """        
-        self.root_game.reset()
-        self.root = self.root.get_root()
+        self.curr_game.reset()
+        self.curr_node = self.root
         i = 0
         while i<num_games:
-            self.search(10000)
             move = self.best_move()
             if move == None:
                 print("Game",i,"over")
-                print("result: ",self.root_game.lose1," ", self.root_game.lose2)
+                print("result: ",self.curr_game.lose1," ", self.curr_game.lose2)
                 i+=1
-                self.root_game.reset()
-                self.root = self.root.get_root()
+                self.curr_game.reset()
+                self.curr_node = self.root
                 continue
-            if self.game_over(self.root_game):
+            if self.game_over(self.curr_game):
                 i+=1
                 print("Game",i,"over")
-                print("result: ",self.root_game.lose1," ", self.root_game.lose2)
+                print("result: ",self.curr_game.lose1," ", self.curr_game.lose2)
+                self.curr_game.reset()
+                self.curr_node = self.root
+                continue
             self.move(move)
 
 
@@ -331,9 +423,9 @@ class MCTS:
         game = s.Surround()
         game.reset()
         board = game.board
-        queue.enqueue((node,board))
-        while not queue.is_empty():
-            node, board = queue.dequeue()
+        queue.put((node,board))
+        while not queue.empty():
+            node, board = queue.get()
             boards_buffer.append(board)
             probs = np.zeros((4,), dtype=np.float32)
             player1 = np.nonzero(board[:,:,1])
@@ -349,11 +441,19 @@ class MCTS:
                 board[player1[0],player1[1],0] = 1
                 board[player2[0],player2[1],0] = 1
                 probs[child.move[0]-1] += child.value()[0]
-                queue.enqueue((child,child_board))
+                queue.put((child,child_board))
             probs_buffer.append(probs)
 
 
         return np.array(boards_buffer), np.array(probs_buffer)
+    
+    def __sizeof__(self) -> int:
+        """returns the size of the tree
+
+        :return: size of the tree
+        :rtype: int
+        """        
+        return self.node_count
     
     def get_game(self,node = None):
 
@@ -377,22 +477,23 @@ class MCTS:
     
 if __name__ == "__main__":
     smmcts = MCTS()
-    smmcts.train(50)
-    with open("tree.pkl","wb") as f:
-        pickle.dump(smmcts,f)
-    # x = input("Let's play")
-    # jogo = s.Surround(human_render=True, human_controls=0, frame_rate=10)
-    # jogo.reset()
-    # x = 1
-    # tempo = time.time()
-    # while x < 10000:
-    #     y = 1
-    #     while y < 100:
-    #         if smmcts.best_move() != None:
-    #             move = smmcts.best_move()
-    #         reward, old_board, board, lose1, lose2 = jogo.step(move)
-    #         smmcts.move(smmcts.best_move())
-    #         y += 1
-    #     x += 1
-    #     print(x)
+    smmcts.search(10000)
+
+
+    x = input("Let's play")
+    jogo = s.Surround(human_render=True, human_controls=0, frame_rate=10)
+    jogo.reset()
+    x = 1
+    tempo = time.time()
+    while x < 10000:
+        y = 1
+        while y < 100:
+            move = (1,1)
+            if smmcts.best_move() != None:
+                move = smmcts.best_move()
+            reward, old_board, board, lose1, lose2 = jogo.step(move)
+            smmcts.move(move)
+            y += 1
+        x += 1
+        print(x)
     

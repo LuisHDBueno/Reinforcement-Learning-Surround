abc1bb6a63aef4ee73eeeacd269fd0e59c2b4cbb
Vinícius Sousa <vinicius.antunes@proton.me>
2023-10-18 11:20:29 -0300

    Delete teste.py

--- models/teste.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-# matriz de permutacao 16X16 de todas as linhas e colunas
-# para ser usada na funcao de rotacao
-permutation_matrix = np.zeros((16, 16))
-for i in range(16):
-    permutation_matrix[i, 15 - i] = 1
-
-print(permutation_matrix)

3a677ff3d40f8bc100bafd360b9dd0f62326b6e4
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2023-10-17 22:38:36 -0300

    Update run_me.py

--- game/run_me.py
+++ game/run_me.py
@@ -1,4 +1,5 @@
 import surround as s
+import numpy as np
 import os
 import sys
 
@@ -11,7 +12,7 @@ if __name__ == "__main__":
 
     if len(parameters) > 1:
         raise Exception("Expected only one parameter, got " + str(len(parameters)))
-    elif len(parameters) == 0:
+    elif len(parameters) == 0 or parameters[0] == "human":
         game = s.Surround(human_render=True, human_controls=2, frame_rate=5)
         game.reset()
 
@@ -20,11 +21,31 @@ if __name__ == "__main__":
                 game.step((0, 0))
                 if game.lose1 or game.lose2:
                     break
+
     elif parameters[0] == "challenge":
-        pass
-    elif parameters[0] == "watch":
-        pass
-    elif parameters[0] == "human":
-        pass
+        game = s.Surround(human_render=True, human_controls=1, frame_rate=5)
+
+    elif parameters[0] == "random":
+        game = s.Surround(human_render=True, human_controls=0, frame_rate=5)
+        game.reset()
+
+        model = DenseNet()
+
+        for _ in range(10):
+            while True:
+                moves1, moves2 = model.legal_moves(game)
+
+                a = 0
+                b = 0
+
+                if len(moves1) > 0:
+                    a = moves1[np.random.randint(len(moves1))]
+                if len(moves2) > 0:
+                    b = moves2[np.random.randint(len(moves2))]
+
+                game.step((a, b))
+                if game.lose1 or game.lose2:
+                    break
+
     else:
         raise Exception("Invalid parameter: " + parameters[0])
